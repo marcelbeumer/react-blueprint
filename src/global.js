587f44c202ac548874df3da312203c24ebf5621e
Marcel Beumer <marcel@marcelbeumer.com>
2016-02-05 11:42:57 +0100

    Comment

--- src/global.js
+++ src/global.js
@@ -1,5 +1,8 @@
 const ROOT_OBJECT = '__';
 
+// evil module: instead, rewrite to do something similar to what the
+// debug module does
+
 export function expose(name, thing) {
   global[ROOT_OBJECT] = global[ROOT_OBJECT] || {};
   global[ROOT_OBJECT][name] = thing;

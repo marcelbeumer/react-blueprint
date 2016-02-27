9ca0440c24e56a99f4e5ad5de38016985a84dc04
Marcel Beumer <marcel@marcelbeumer.com>
2016-02-27 02:07:57 +0100

    Stilr setup

--- src/component/styles.js
+++ src/component/styles.js
@@ -1,33 +1,11 @@
-import jss from 'jss';
-const sheets = [];
+import { render, create } from 'stilr';
 
-const camelCaseRegex = /([a-z]|^)([A-Z])/g;
-
-export function translatePropertyCasing(props = {}) {
-  const processed = {};
-  Object.keys(props).forEach(key => {
-    const dashedKey = key.replace(camelCaseRegex, '$1-$2').toLowerCase();
-    processed[dashedKey] = props[key];
-  });
-  return processed;
-}
-
-export function styleSheet(styles = {}) {
-  const translated = Object.assign({}, styles);
-  Object.keys(translated).forEach(selector =>
-    translated[selector] = translatePropertyCasing(translated[selector]));
-
-  const sheet = jss.createStyleSheet(translated);
-  sheets.push(sheet);
-  return Object.assign({}, sheet.classes, { getStyles: () => styles });
-}
-
-export function getCss() {
-  return sheets.map(styles => styles.toString()).join('\n');
-}
+export const getCss = () => render();
 
 export default class StyleSheet {
   static create(styles) {
-    return styleSheet(styles);
+    const sheet = create(styles);
+    sheet.getSource = () => styles;
+    return sheet;
   }
 }

cd9a0c75dea5cd5d3ffa842cec60f1fbafd65862
Marcel Beumer <marcel@marcelbeumer.com>
2016-02-27 03:37:15 +0100

    Use local map for component rendering

--- src/component/styles.js
+++ src/component/styles.js
@@ -1,10 +1,12 @@
-import { render, create } from 'stilr';
+import { render, create, Map } from 'stilr';
 
-export const getCss = () => render();
+const map = new Map();
+
+export const getCss = () => render(undefined, map);
 
 export default class StyleSheet {
   static create(styles) {
-    const sheet = create(styles);
+    const sheet = create(styles, map);
     sheet.getStyles = () => styles;
     return sheet;
   }

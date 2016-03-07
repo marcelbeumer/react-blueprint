4defe13759155dc5395637113ebca8af7988124c
Marcel Beumer <marcel@Boxer.local>
2016-03-07 20:21:47 +0100

    Load server module and render homepage

--- test/test.js
+++ test/test.js
@@ -1,8 +1,15 @@
 /* eslint-env node, mocha */
 import assert from 'assert';
+import { renderHomepage } from '../src/server';
 
-describe('test example', () => {
-  it('does nothing yet', () => {
-    assert(true);
+describe('server module', () => {
+  const html = renderHomepage();
+
+  it('renders the homepage as string', () => {
+    assert(typeof(html) === 'string');
+  });
+
+  it('renders the homepage as html', () => {
+    assert(/<html/.test(html));
   });
 });

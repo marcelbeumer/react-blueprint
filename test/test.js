8d8642d0307c512055fc992070f8ad256a2e8b29
Marcel Beumer <marcel@Boxer.local>
2016-03-22 12:21:22 +0100

    Update test

--- test/test.js
+++ test/test.js
@@ -1,21 +1,31 @@
 /* eslint-env node, mocha */
 import assert from 'assert';
-import { renderApp } from '../src/server';
+import { renderApp, staticApp } from '../src/server';
 
 describe('server module', () => {
-  let html;
+  describe('browser rendering', () => {
+    let html;
 
-  beforeEach(() =>
-    renderApp('/')
-      .then(out => {
-        html = out;
-      }));
+    beforeEach(() => staticApp('/').then(out => {
+      html = out;
+    }));
 
-  it('renders the homepage as string', () => {
-    assert(typeof(html) === 'string');
+    it('returns a html string', () => {
+      assert(/<html/.test(html));
+    });
   });
 
-  it('renders the homepage as html', () => {
-    assert(/<html/.test(html));
+  describe('server rendering', () => {
+    let html;
+
+    beforeEach(() => renderApp('/').then(out => {
+      html = out;
+    }));
+
+    it('renders a html string containing react elements', () => {
+      assert(/<html/.test(html));
+      assert(/data-react-checksum/.test(html));
+      assert(/data-reactid/.test(html));
+    });
   });
 });

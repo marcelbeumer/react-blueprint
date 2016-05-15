99efd56b3f4ba08cb5df13637f51ef30fbf1dc06
Marcel Beumer <marcel@marcelbeumer.com>
2016-05-15 10:27:09 +0200

    Move component css generation from server to standalone/dev-server

---
+++ src/component-css.js
@@ -0,0 +1,12 @@
+/* eslint global-require:0 */
+import CleanCSS from 'clean-css';
+import autoprefixer from 'autoprefixer';
+import postcss from 'postcss';
+
+export default function getComponentCss(minify: boolean): string {
+  require('./component');
+  const { getCss } = require('./component/styles');
+  const source = getCss({ pretty: !minify });
+  const css = String(postcss([autoprefixer]).process(source));
+  return minify ? new CleanCSS().minify(css).styles : css;
+}

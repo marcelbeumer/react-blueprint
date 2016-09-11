262cdcb2c510427c9609c9e6b956231e4e79efaf
Marcel Beumer <marcel@marcelbeumer.com>
2016-09-11 18:21:02 +0200

    Flow

--- src/renderer/base.js
+++ src/renderer/base.js
@@ -1,8 +1,9 @@
+// @flow
 import React from 'react'; // eslint-disable-line no-unused-vars
 import { Provider } from 'react-redux';
 import App from '../component';
 
-export default function render(store) {
+export default function render(store: Object): React.Element<*> {
   return (
     <Provider store={store}>
       <App />
--- src/renderer/server.js
+++ src/renderer/server.js
@@ -1,7 +1,8 @@
+// @flow
 import React from 'react'; // eslint-disable-line no-unused-vars
 import ReactDOMServer from 'react-dom/server';
 import baseRenderer from './base';
 
-export default function render(store: Object) {
+export default function render(store: Object): string {
   return ReactDOMServer.renderToString(baseRenderer(store));
 }
